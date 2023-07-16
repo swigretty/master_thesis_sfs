d143d8b998f56d47a49ab63565cec2fd6831c333
gianna <gia.mar.yae@gmail.com>
2023-07-16 12:41:35 +0200

    baseline metho

--- gp/baseline_methods.py
+++ gp/baseline_methods.py
@@ -2,13 +2,15 @@ import numpy as np
 from gp.gp_data import GPData
 
 
-def pred_empirical_mean(x, y):
+def pred_empirical_mean(x_pred, x_train, y_train):
     """
     Using the overall mean as prediction.
     cov is calculated assuming iid data.
     """
-    sigma_mean = 1 / len(y) * np.var(y)
-    y_cov = np.zeros((len(x), len(x)), float)
+    sigma_mean = 1 / len(y_train) * np.var(y_train)
+    y_cov = np.zeros((len(x_pred), len(x_pred)), float)
     np.fill_diagonal(y_cov, sigma_mean)  # iid assumption
-    y = np.repeat(np.mean(y), len(x))
-    return GPData(x=x, y_mean=y, y_cov=y_cov)
+    y = np.repeat(np.mean(y_train), len(x_pred))
+    return GPData(x=x_pred, y_mean=y, y_cov=y_cov)
+
+
