fixed measurment noise scaling

exploration/explore_kernels.py
@@ -82,11 +82,12 @@ def plot_kernels(kernels, t=np.linspace(0, 20, 200), plot_file=None, mode_values
         plot_sample_path(t, k, ax=ax[1], nsim=nsim)
 
     if mode_values is not None:
-        plot_sim_info(kernels, mode_values=mode_values, mode_name=mode_name, t=t, ax=ax[2])
+        sim_info = plot_sim_info(kernels, mode_values=mode_values, mode_name=mode_name, t=t, ax=ax[2])
 
     [a.legend() for a in ax]
 
     fig.savefig(plot_path / plot_file)
+    return sim_info
 
 
 def plot_sim_info(kernels, mode_values, mode_name="mode", t=np.linspace(0, 20, 200), nsim=1000, ax=None):
@@ -101,7 +102,7 @@ def plot_sim_info(kernels, mode_values, mode_name="mode", t=np.linspace(0, 20, 2
         ax.plot(sim_info_df[mode_name], sim_info_df["max-min"], label="max-min")
         ax.set_xlabel(mode_name)
 
-    return sim_info_df
+    return sim_info
 
 
 if __name__ == "__main__":
@@ -136,15 +137,16 @@ if __name__ == "__main__":
     # kernels = [Matern(length_scale=3, nu=0.5) * c for c in var]
     # plot_kernels(kernels, mode_name="rbf50_var", t=t, mode_values=var)
 
-    # kernels = [ExpSineSquared(length_scale=3, periodicity=h_per_day) * c for c in var]
-    # plot_kernels(kernels, t=t, mode_name="sin3_var", mode_values=var, nsim=100)
+    kernels = [ExpSineSquared(length_scale=3, periodicity=h_per_day) * c for c in var]
+    sim_info = plot_kernels(kernels, t=t, mode_name="sin3_var", mode_values=var, nsim=1000)
+    print("Hello")
 
     # kernels = [WhiteKernel(noise_level=c)for c in var]
     # plot_kernels(kernels, t=t, mode_name="white_var", mode_values=var)
     #
-    kernels = [14**2 * ExpSineSquared(length_scale=3, periodicity=h_per_day) + Matern(
-        length_scale=1, nu=0.5) * 5 + RBF(length_scale=50) * 5]
-    plot_kernels(kernels, t=t, mode_name="sin_rbf", nsim=10000)
+    # kernels = [14**2 * ExpSineSquared(length_scale=3, periodicity=h_per_day) + Matern(
+    #     length_scale=1, nu=0.5) * 5 + RBF(length_scale=50) * 5]
+    # plot_kernels(kernels, t=t, mode_name="sin_rbf", nsim=10000)
 
 
 

gp/gp_experiments.py
@@ -51,7 +51,7 @@ def evaluate_data_fraction(mode_config, data_fraction=(0.1, 0.2, 0.4),
     experiment_output_path = get_output_path(experiment_name=experiment_name)
     target_measures_path = experiment_output_path / f"target_measures_eval.csv"
 
-    session_name = f"{mode_config.kernel_sim_name}"
+    session_name = f"{mode_config.session_name}"
     output_path_gp_sim = partial(get_output_path, session_name=session_name,
                                  experiment_name=experiment_name)
 
@@ -97,7 +97,7 @@ def evaluate_data_fraction_modes(modes, data_fraction=(0.1, 0.2, 0.4), meas_nois
     experiment_output_path = get_output_path(experiment_name=experiment_name)
 
     for nv in meas_noise_var:
-        for mode_config in modes():
+        for mode_config in modes:
             if nv is not None:
                 mode_config = mode_config(meas_noise_var=nv)
             else:
@@ -131,16 +131,22 @@ def plot_sample(normalize_kernel=True, experiment_name="test_single_sample", rng
 if __name__ == "__main__":
     setup_logging()
 
-    modes = [partial(GPSimulatorConfig, kernel_sim_name="sin_rbf"),
-             partial(GPSimulatorConfig, kernel_sim_name="sin_rbf", data_fraction_weights=lambda x: x ** 1)]
+    modes = [
+        # partial(GPSimulatorConfig, kernel_sim_name="sin_rbf",
+        #              session_name="sin_rbf_default"),
+             partial(GPSimulatorConfig, kernel_sim_name="sin_rbf", data_fraction_weights=lambda x: x ** 1,
+                     session_name="sin_rbf_seasonal_default"),
+             partial(GPSimulatorConfig, kernel_sim_name="sin_rbf", data_fraction_weights=lambda x: x ** 2,
+                     session_name="sin_rbf_seasonal_extreme")
+             ]
 
     rng = np.random.default_rng(18)
-    experiment_name = "data_fraction_default"
+    experiment_name = "data_fraction_seasonal"
 
     # plot_sample(normalize_kernel=False, rng=rng, experiment_name=experiment_name, nplots=1,
     #             config=GPSimulatorConfig(kernel_sim_name="sin_rbf"), data_fraction=0.2)
-    evaluate_data_fraction(GPSimulatorConfig(kernel_sim_name="sin_rbf"),
-                           experiment_name=experiment_name, n_samples=100, data_fraction=(0.1, ))
-    # evaluate_data_fraction_modes(modes, n_samples=2, experiment_name="default_modes")
+    # evaluate_data_fraction(GPSimulatorConfig(kernel_sim_name="sin_rbf"),
+    #                        experiment_name=experiment_name, n_samples=100, data_fraction=(0.1, ))
+    evaluate_data_fraction_modes(modes, n_samples=100, experiment_name=experiment_name)
 
 