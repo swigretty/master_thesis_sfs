53e51b5507f03d2aeeb4ee48c50d9333ad1a2d8b
gianna <gia.mar.yae@gmail.com>
2023-09-21 10:10:16 +0200

    Documentation

--- gp/gp_experiments.py
+++ gp/gp_experiments.py
@@ -1,5 +1,9 @@
+"""
+This modules stores functionality to run the main experiments
+to assess performance of GP regression and some baseline methods
+in estimating simulated BP values.
+"""
 import copy
-
 from gp.gp_simulator import GPSimulator, GPSimulationEvaluator
 from gp.simulate_gp_config import GPSimulatorConfig
 from logging import getLogger
@@ -27,10 +31,34 @@ def get_sample_path_variance_kernel(kernel, t, nsim=1000):
     return sim_info_mean["var"]
 
 
-def evaluate_data_fraction(mode_config, data_fraction=(0.05, 0.1, 0.2, 0.4),
-                           n_samples=100, experiment_name="test",
-                           normalize_kernel=False,
-                           normalize_y=True, only_var=False, seed=15):
+def evaluate_data_fraction(
+        mode_config: GPSimulatorConfig,
+        data_fraction: tuple = (0.05, 0.1, 0.2, 0.4),
+        n_samples: int = 100, experiment_name: str = "test",
+        normalize_kernel: bool = False,
+        normalize_y: bool = True, seed: int = 15):
+    """
+    Run simulation for different data fractions. Makes sure that the same
+    samples are drawn from the GP for every data fraction.
+
+    Keyword arguments:
+
+    normalyze_y: This will mean center and scale to unit variance the simulated
+    data produced before fitting the GP regression. This is important for
+    the optimizer used when optimizing the hyperparameters. If set to False
+    results will generally be worse. However, when set to True
+    the found hyperparameters during GP regression cannot be compared to the
+    ones used during simulation anymore.
+
+    normalize_kernel: Experimental, you generally want to set this to False.
+    If set to True this will modify the simulation kernel specified in
+    mode_config.kernel_sim such that it will produce sample paths
+    that have close to unit variance. You may want to set to True if you
+    want to be able to compare the simulated kernel to the fitted kernel
+    hyperparameters. However, in such cases you should set normalize_y to False.
+
+    """
+
     mode_config_orig = mode_config.to_dict()
     mode_config_norm = copy.copy(mode_config_orig)
 
@@ -63,22 +91,21 @@ def evaluate_data_fraction(mode_config, data_fraction=(0.05, 0.1, 0.2, 0.4),
         simulator.plot_gp_regression_sample(nplots=1)
         simulator.evaluate()
         simulator.evaluate_target_measures()
-        eval_dict, measure_sum_df = simulator.evaluate_multisample(
-            n_samples, only_var=only_var)
-
-        if only_var:
-            return
+        eval_dict, measure_sum_df = simulator.evaluate_multisample(n_samples)
 
         for k, v in simulator.current_init_kwargs.items():
             if not isinstance(v, np.ndarray):
                 measure_sum_df[k] = v
 
+        # Write the evaluation summary for the target measures to csv
         if not (target_measures_path).exists():
             measure_sum_df.to_csv(target_measures_path, index=None)
         else:
             measure_sum_df.to_csv(target_measures_path, mode='a', header=False,
                                   index=None)
 
+        # General performanc metrics only calculated for the GP regression
+        # Stores test, train and overall performance in different tables
         for k, v in eval_dict.items():
             df = pd.DataFrame([v])
             df["session_name"] = session_name
@@ -91,10 +118,13 @@ def evaluate_data_fraction(mode_config, data_fraction=(0.05, 0.1, 0.2, 0.4),
     return df
 
 
-def plot_sample(normalize_kernel=False, experiment_name="test_single_sample",
-                rng=None,
-                nplots=1, config=GPSimulatorConfig(), data_fraction=0.2,
-                normalize_y=True, plot_method=None):
+def plot_sample(normalize_kernel: bool = False,
+                experiment_name: str = "test_single_sample",
+                rng: np.random.Generator = None, nplots: int = 1,
+                config: GPSimulatorConfig = GPSimulatorConfig(),
+                data_fraction: float = 0.2,
+                normalize_y: bool = True, plot_method: str = None):
+
     output_path_gp_sim = partial(get_output_path,
                                  session_name=config.session_name,
                                  experiment_name=experiment_name)
@@ -122,18 +152,19 @@ if __name__ == "__main__":
 
     modes = MODES
 
-    rng = np.random.default_rng(18)
+    # Run main Simulation Experiment accross different data fractions
+    # and sampling patterns (modes).
     experiment_name = "my_experiment"
-
     for mode in modes:
         evaluate_data_fraction(mode,
                                n_samples=100,
                                experiment_name=experiment_name,
                                normalize_kernel=False,
-                               normalize_y=True)
+                               normalize_y=True, seed=15)
 
+    # Generate plots visualizing predictions of regression methods
+    # fitted to data based on different samples drawn form the true GP
     experiment_name = "my_plots"
-
     for datafrac in [0.05, 0.1, 0.2, 0.4]:
         for mode in modes:
             mode.session_name = f"{mode.session_name}_{datafrac}"
--- gp/simulate_gp_config.py
+++ gp/simulate_gp_config.py
@@ -1,6 +1,6 @@
-from sklearn.gaussian_process.kernels import RBF,  WhiteKernel, ExpSineSquared, ConstantKernel, RationalQuadratic, \
-    Matern, ConstantKernel, DotProduct
-from dataclasses import dataclass, asdict
+from sklearn.gaussian_process.kernels import (
+    RBF, WhiteKernel, ExpSineSquared, Matern, ConstantKernel, DotProduct)
+from dataclasses import dataclass
 from logging import getLogger
 import numpy as np
 from log_setup import setup_logging
@@ -18,11 +18,13 @@ def mean_fun_const(x):
     return 120
 
 
-PARAM_NAMES = ["noise_level", "length_scale", "constant_value", "periodicity", "sigma_0", "nu"]
+PARAM_NAMES = ["noise_level", "length_scale", "constant_value",
+               "periodicity", "sigma_0", "nu"]
 
 # Simple Kernels
 simple_kernel_config = {
-    "white": {"kernel": WhiteKernel, "params": {"noise_level": 1}, "bound_params": {}, "var": 1,
+    "white": {"kernel": WhiteKernel, "params": {"noise_level": 1},
+              "bound_params": {}, "var": 1,
               "var_bounds": (0.1, 10)},
     "ou": {"kernel": Matern, "params": {"length_scale": 3, "nu": 0.5},
            "bound_params": {"length_scale_bounds": (0.1, 10)},
@@ -30,7 +32,8 @@ simple_kernel_config = {
     "rbf_long": {"kernel": RBF, "params": {"length_scale": 50},
                  "bound_params": {"length_scale_bounds": (1, 200)},
                  "var": 5, "var_bounds": (0.1, 100)},
-    "rbf_short": {"kernel": RBF, "params": {"length_scale": 3}, "bound_params": {"length_scale_bounds": (0.1, 20)},
+    "rbf_short": {"kernel": RBF, "params": {"length_scale": 3},
+                  "bound_params": {"length_scale_bounds": (0.1, 20)},
                   "var": 1, "var_bounds": (0.1, 10)},
     "rbf_medium": {"kernel": RBF, "params": {"length_scale": 25},
                    "bound_params": {"length_scale_bounds": (1, 200)},
@@ -40,59 +43,30 @@ simple_kernel_config = {
         "periodicity_bounds": "fixed", "length_scale_bounds": (0.1, 20)},
                 "var": 14**2, "var_bounds": (10, 1000)},
     # Note sample_ampl = sqrt(2*sample_var)
-    # var of 100 leads to sample_var: 4.17, sample_ampl: 2.89 (overall max-min = 5.78)
-    # var of 198 (=14.1**2) leads to sample_var: 13.28, sample_ampl: 5.15 (overall max-min = 5.78)
-
-    # "sin_week": {"kernel": ExpSineSquared, "params": {
-    #            "length_scale": 3, "periodicity": PERIOD_WEEK}, "bound_params": {"periodicity_bounds": "fixed"},
-    #              "var": 5},
+    # var of 100 leads to sample_var: 4.17,
+    # sample_ampl: 2.89 (overall max-min = 5.78)
+    # var of 198 (=14.1**2) leads to sample_var: 13.28,
+    # sample_ampl: 5.15 (overall max-min = 5.78)
     "dot": {"kernel": DotProduct, "params": {"sigma_0": 0.001}, "bound_params": {"sigma_0_bounds": (0.0001, 0.01)},
             "var": 0.01, "var_bounds": (0.001, 1)}
            }
 
-KERNELS = {}
-OU_KERNELS = {}
 
-for mode in ["fixed", "bounded", "unbounded"]:
-
-    if mode == "unbounded":
-        _simple_kernels = {k: ConstantKernel(constant_value=v["var"]) * v["kernel"](
-            **{param: "fixed" for param in v["kernel"]().get_params().keys() if param == "periodicity_bounds"},
-            **v["params"]) for k, v in simple_kernel_config.items()}
-    if mode == "bounded":
-        # Kernels with bounds defined in simple_kernel_config
-        _simple_kernels = {k: ConstantKernel(
-            constant_value=v["var"], constant_value_bounds=v["var_bounds"]) * v["kernel"](
-            **v["bound_params"], **v["params"]) for k, v in simple_kernel_config.items()}
-    if mode == "fixed":
-        # Fixed Kernels, bounds form simple_kernel_config are ignored
-        _simple_kernels = {k: ConstantKernel(constant_value=v["var"], constant_value_bounds="fixed") * v["kernel"](
-                **{param: "fixed" for param in v["kernel"]().get_params().keys() if "bounds" in param},
-                **v["params"]) for k, v in simple_kernel_config.items()}
-
-    _combination_kernels = {
-        "sinrbf": _simple_kernels["sin_day"] * _simple_kernels["rbf_long"],
-        "sinrbf_rbf":  _simple_kernels["sin_day"] * _simple_kernels["rbf_medium"] +
-                       _simple_kernels["rbf_long"],
-                            "sin_rbf": _simple_kernels["sin_day"] + _simple_kernels["rbf_long"]
-                            }
-    _kernels = {**_simple_kernels, **_combination_kernels}
-
-    ou_kernels = {k: (_kernels["ou"] + v if k != "ou" else v)
-                  for k, v in _kernels.items()}
-
-    KERNELS[mode] = _kernels
-    OU_KERNELS[mode] = ou_kernels
-
-
-# measuring time in hours
 @dataclass()
 class GPSimulatorConfig():
+    """
+    This class can be used to obtain the configuration for
+    gp.gp_simulator.GPSimulationEvaluator.
+    Example usage:
+    gpe = gp.gp_simulator.GPSimulationEvaluator(
+                        **GPSimulatorConfig().to_dict())
+    """
 
     n_days: int = 7
     samples_per_hour: int = 10
-    meas_noise_var: float = 62 / 2  # Meas noise std = 7.9, leads to noise_var=62.
-    # Var(Noise_CUFF - Noise_Aktiia) = Var(Noise_CUFF) + Var(Noise_Aktiia) - 2COV(Noise_CUFF - Noise_Aktiia)
+
+    # Measurement noise variance is half of Var(Noise_CUFF - Noise_Aktiia) = 62
+    meas_noise_var: float = 62 / 2
     mean_f: callable = mean_fun_const
     kernel_sim_name: str = "sin_rbf"
 
@@ -114,7 +88,8 @@ class GPSimulatorConfig():
 
     kwargs: dict = None
 
-    simulation_config_keys = ["mean_f", "x", "meas_noise_var", "kernel_sim", "data_fraction_weights"]
+    simulation_config_keys = ["mean_f", "x", "meas_noise_var", "kernel_sim",
+                              "data_fraction_weights"]
 
     def __post_init__(self):
         for k_name in ["sin", "rbf", "ou"]:
@@ -133,17 +108,26 @@ class GPSimulatorConfig():
 
     @property
     def kernel_sim(self):
+        """
+        The kernel to be used for simulating the true BP values
+        """
         if self.kernel_sim_name == "sin_rbf":
             return self.ou_kernel + self.sin_kernel + self.rbf_kernel
 
         elif self.kernel_sim_name == "sinrbf_rbf":
-            return self.ou_kernel + self.sin_kernel * self.rbf_kernel + self.rbf_kernel
+            return (self.ou_kernel + self.sin_kernel * self.rbf_kernel +
+                    self.rbf_kernel)
 
         else:
             raise ValueError(f"{self.kernel_sim_name=} does not exist")
 
     @property
     def x(self):
+        """
+        The time points, at which we are going to simulate and predict BP
+        values. It spans one week with 10 points per hour and is measured
+        in hours.
+        """
         return np.linspace(0, PERIOD_DAY * self.n_days,
                            PERIOD_DAY * self.n_days * self.samples_per_hour)
 
